ea6181fa6c35f0cdd43d5f029075d4e3d058fbf4
Callum Dickinson <callum@huttradio.co.nz>
2015-09-02 08:50:39 +1200

    Boilerplate for check functions in the REST API

--- api/index.php
+++ api/index.php
@@ -1,5 +1,36 @@
 <?php
 
-phpinfo();
+function check_db($site_name)
+{
+	$ret = false;
+
+	$host = "db.kohaaas.catalyst.net.nz";
+	$username = "kohaaas.catalyst.net.nz";
+	$passwd = "kohaaas.catalyst.net.nz";
+
+	$conn = mysqli_connect($host, $username, $passwd, $database);
+
+	if ($ret)
+	{
+		mysqli_close($conn);
+		$ret = true;
+	}
+
+	return $ret;
+}
+
+function check_cachedb($site_name)
+{
+	$ret = false;
+
+	return $ret;
+}
+
+function check_koha($site_name)
+{
+	$ret = false;
+
+	return $ret;
+}
 
 ?>
