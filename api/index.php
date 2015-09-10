59023b9d0e1e3fe2978cf0b1ec662130379c46a2
Callum Dickinson <callum@huttradio.co.nz>
2015-09-11 01:23:24 +1200

    TODO: use a data structure to store scheduled registrations, and do them all at once

--- api/index.php
+++ api/index.php
@@ -1,6 +1,105 @@
 <?php
 
 ##
+# Use extensions.
+##
+
+# PECL extensions: yaml, ssh2, phalcon
+use Phalcon\Mvc\Micro;
+
+##
+# Global static configuration variables.
+##
+
+const LOCAL_SITES_DIR = "/tmp/koha-sites";
+const LOCAL_SITES_DIR_CREATE_MODE = 0600;
+
+const PUPPET_MASTER = "puppet.kohaaas.catalyst.net.nz";
+const PUPPET_MASTER_KOHA_SITES_DIR = "/vol/koha-sites";
+const PUPPET_MASTER_KOHA_SITES_CREATE_MODE = 0440;
+
+##
+# Classes.
+##
+
+class Site
+{
+	private $first_name;
+	private $surname;
+
+	private $site_name;
+
+	private $base_domain;
+
+	private $email;
+	private $password;
+
+	function __construct($args)
+	{
+		$this->first_name = $args[0];
+		$this->surname = $args[1];
+		$this->site_name = $args[2];
+		$this->base_domain = $args[3];
+		$this->email = $args[4];
+		$this->password = $args[5];
+	}
+
+	function export()
+	{
+		$data = array
+		(
+			"first_name"	=> $this->first_name,
+			"surname"	=> $this->surname,
+			"site_name"	=> $this->site_name,
+			"base_domain"	=> $this->base_domain,
+			"email"		=> $this->email,
+			"password"	=> $this->password,
+		);
+
+		$local_file = LOCAL_SITES_DIR . "/{$this->site_name}.yaml";
+		$remote_file = PUPPET_MASTER_KOHA_SITES_DIR . "/{$this->site_name}.yaml";
+
+		# Create the temporary directory (if it doesn't already exist), and set the correct permissions on it.
+		mkdir(LOCAL_SITES_DIR, LOCAL_SITES_DIR_CREATE_MODE);
+
+		# Create the yaml file with the site's information.
+		yaml_emit_file($local_file, $data);
+
+		# Open the SSH connection to the Puppet Master.
+		$session = ssh2_connect(PUPPET_MASTER);
+		ssh2_scp_send($session, $local_file, $remote_file, PUPPET_MASTER_KOHA_SITES_CREATE_MODE);
+
+		# Delete the local copy of the yaml file.
+		unlink($local_file);
+	}
+
+	function check_db()
+	{
+		return !empty(system("puppet query nodes 'Koha::Db::Site[\"$site_name\"]'"));
+	}
+
+	function check_zebra()
+	{
+		return !empty(system("puppet query nodes 'Koha::Zebra::Site[\"$site_name\"]'"));
+	}
+
+	function check_memcached()
+	{
+		return !empty(system("puppet query nodes 'Koha::Memcache::Site[\"$site_name\"]'"));
+	}
+
+	function check_koha()
+	{
+		return !empty(system("puppet query nodes 'Koha::Koha::Site[\"$site_name\"]'"));
+	}
+
+	function check_proxy()
+	{
+		return !empty(system("puppet query nodes 'Koha::Proxy::Site[\"$site_name\"]'"));
+	}
+}
+
+##
 # Helper functions.
 ##
 
@@ -9,21 +108,13 @@ function log_print($text)
 	file_put_contents("/var/log/apache2/api.log", $text, FILE_APPEND | LOCK_EX);
 }
 
-##
-# The REST API to collect site information from users.
-##
-
-##
-# Update the instances.
-##
-
 function update($command)
 {
 	$date = date(DATE_COOKIE);
 	$log = system($command, $ret);
 
 	if ($ret != 0)
-		log_print("Command: $command\nDate: $date\nReturn code: $ret\n\n$log\n\nEnd command.\n-------------\n\n");
+		log_print("##\n# Command: $command\n# Date: $date\n# Return code: $ret\n##\n\n$log\n\n##\n# End command.\n##\n\n-------------\n\n");
 
 	return $ret;
 }
@@ -48,87 +139,60 @@ function update_proxy()
 	return update("mco puppet runonce -C site::roles::proxy", $ret);
 }
 
-##
-# Check if the Koha site components are available.
-##
-
-
-class Site
+function update_all()
 {
-	private $firstname;
-	private $surname;
-
-	private $site_name;
-
-	private $domain;
-
-	private $email;
-	private $pass;
-
-	function __construct($args)
-	{
-		$this->firstname = $args[0];
-		$this->surname = $args[1];
-		$this->site_name = $args[2];
-		$this->domain = $args[3];
-		$this->email = $args[4];
-		$this->pass = $args[5];
-	}
-
-	function export()
-	{
-		# scp the yaml file to the Puppet Master!
-	}
-
-	function check_db()
-	{
-		$ret = false;
-
-		$host = ;
-		$username = ;
-		$passwd = ;
-
-		$conn = mysqli_connect($host, $username, $passwd, $database);
-
-		if ($conn)
-		{
-			mysqli_close($conn);
-			$ret = true;
-		}
-
-		return $ret;
-	}
-
-	function check_zebra()
-	{
-		$ret = false;
-
-
-		return $ret;
-	}
-
-	function check_memcached()
-	{
-		$memcached = new Memcached;
-
-		$ret = $memcached->addServer();
-		$memcached->resetServerList();
-		return new ;
-	}
-
-	function check_koha()
-	{
-		$ret = false;
-
-		return $ret;
-	}
-
-	function check_proxy()
-	{
-		$ret = false;
-
-		return $ret;
-	}
+	update_db();
+	update_cachedb();
+	update_koha();
+	update_proxy();
 }
+
+function update_schedule()
+{
+	
+}
+
+##
+# The REST API to collect site information from users.
+##
+
+##
+# Update the instances.
+##
+
+$app = new Micro();
+
+$app->get("/api/register", function ()
+{
+	# Get the new site information from the JSON object.
+	
+	# If the parameters are correct.
+	{
+		# Create a site object with the registered site's information, and export
+		# it to the Puppet Master.
+		$site = new Site()
+		$site.export();
+
+		# Schedule an update of the instances.
+		update_schedule();
+
+		# Start building the response.
+		$response = new Response();
+
+		# 202 Accepted
+		if ($ret == 0)
+		{
+			$response->setStatusCode(202, "Accepted");
+			$response->setJsonContent("session" => $session);
+		}
+	}
+	# 400 Bad Request
+	# 
+	else
+	{
+	}
+});
+
+$app->handle();
 
 ?>
